a31df00fea86f9d196919551681b51ac1ef31497
Maksim Shaynyuk <35791051+maxshay@users.noreply.github.com>
2023-06-16 22:55:18 -0700

    update config, logo

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
-// import adapter from '@sveltejs/adapter-auto';
 import adapter from "@sveltejs/adapter-node"
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-debian-11
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// https://kit.svelte.dev/docs/integrations#preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
